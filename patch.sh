79ab8774da2b52a7015d1dc137998d3d1b440ecc
fonylew <kamolphan@hotmail.com>
2015-05-28 12:01:06 +0700

    add aliases.db

--- patch.sh
+++ patch.sh
@@ -7,10 +7,12 @@ rm /usr/local/etc/mail/spamassassin/local.cf
 
 ln ./main.cf /usr/local/etc/postfix/
 ln ./master.cf /usr/local/etc/postfix/
-ln ./amvisd.conf /usr/local/etc/
+ln ./amavisd.conf /usr/local/etc/
 ln ./local.cf /usr/local/etc/mail/spamassassin/
 
 #handling users in vitrual
 ln ./virtual /usr/local/etc/postfix/
+postmap /etc/postfix/virtual
+ln ./aliases.db /etc/
 
 touch /var/log/local0
