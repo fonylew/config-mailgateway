dc7d69b1537992ca79bfdf6b0e9fbbb1b8a00af4
fonylew <kamolphan@hotmail.com>
2015-06-02 13:01:12 +0700

    fixcron again

--- fixcrontab.sh
+++ fixcrontab.sh
@@ -2,12 +2,13 @@ host='example.com'
 cat /etc/passwd | grep /home | cut -d: -f1  > users
 while read p; do
   rm tempcron
-  #ln .procmailrc /home/$p/
   #echo $p@$host $p >> /usr/local/etc/postfix/virtual
+  touch /home/$p/user_prefs
+  
   #crontab -u $p -l > tempcron
   echo '#sa-learn' >> tempcron
-  echo '5 * * * *          /usr/local/bin/sa-learn --spam /home/'$p'/Maildir/Spam/{cur,new} 2>&1 > /dev/null'>> tempcron
-  echo '5 * * * *          /usr/local/bin/sa-learn --ham /home/'$p'/Maildir/{cur,new} 2>&1 > /dev/null' >> tempcron
+  echo '*/5 * * * *          /usr/local/bin/sa-learn -p /home/'$p'/user_prefs --spam /home/'$p'/Maildir/Spam/{cur,new} > /dev/null 2>&1'>> tempcron
+  echo '*/5 * * * *          /usr/local/bin/sa-learn -p /home/'$p'/user_prefs --ham /home/'$p'/Maildir/{cur,new} > /dev/null 2>&1' >> tempcron
   crontab -u $p tempcron
 done <users
 #postmap /usr/local/etc/postfix/virtual
